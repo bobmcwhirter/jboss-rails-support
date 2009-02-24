4c284ec76b54907b3ff24114800eaf2529e1be28
Bob McWhirter <bob@fnokd.com>
2009-02-24 18:24:34 -0500

    Add access to injected JBoss Kernel.

---
+++ lib/jboss/kernel.rb
@@ -0,0 +1,14 @@
+
+module JBoss
+  class Kernel
+    def self.kernel=(kernel)
+      @kernel = kernel 
+    end
+
+    def self.lookup(name)
+      entry = @kernel.getRegistry().findEntry(name)      
+      return nil unless entry
+      entry.getTarget()
+    end
+  end
+end
