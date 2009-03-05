d37ffea502a0cfafc37c32d096bbc51621c4a6f1
Bob McWhirter <bob@fnokd.com>
2009-03-05 13:32:50 -0500

    Pulling in stuff (with fixed) from jboss-rails-plugin (now deprecated).

---
+++ lib/tasks/jboss.rake
@@ -0,0 +1,5 @@
+
+require File.dirname( __FILE__ ) + '/rake_utils.rb'
+require File.dirname( __FILE__ ) + '/jboss_as.rb'
+require File.dirname( __FILE__ ) + '/jboss_rails.rb'
+require File.dirname( __FILE__ ) + '/jboss_gems_jdbc.rb'
---
+++ lib/tasks/jboss_as.rb
@@ -0,0 +1,13 @@
+
+namespace :jboss do
+  namespace :as do
+
+    desc "Check your installation of JBoss-Rails"
+    task :check do
+      matching = Dir[ "#{JBoss::RakeUtils.deployers_dir}/jboss-rails*deployer*" ] 
+      raise "No JBoss-Rails deployer installed in #{JBoss::RakeUtils.deployers_dir}" if ( matching.empty? )
+      puts "JBoss-Rails server: #{JBoss::RakeUtils.server_dir}"
+    end
+
+  end
+end
